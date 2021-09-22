f6fce5d3334273050e602498d06a747129bf983b
Othman Alomair <most3mr@live.com>
2021-09-22 12:46:08 +0300

    Every thing works

--- routes/books.js
+++ routes/books.js
@@ -17,6 +17,38 @@ function asyncHandler(cd){
 router.get('/', asyncHandler(async(req,res)=>{
   const books = await Book.findAll();
   res.render('index', {books})
-}));
+}))
+
+/* GET New book */
+router.get('/new', (req,res) => {
+  res.render('new-book')
+})
+
+/* POST New book */
+router.post('/new', asyncHandler(async(req,res)=> {
+  const newBook = await Book.create(req.body)
+  res.redirect(`/books/${newBook.id}`)
+}))
+
+/* GET Single book "Update" */
+router.get('/:id', asyncHandler(async(req,res)=>{
+  const book = await Book.findByPk(req.params.id)
+  res.render('update-book', {book})
+}))
+
+/* POST Single book "Update" */
+router.post('/:id', asyncHandler(async(req,res)=>{
+  const book = await Book.findByPk(req.params.id)
+  await book.update(req.body)
+  res.redirect('/books')
+}))
+
+/* POST DELETE Single book */
+router.post('/:id/delete', asyncHandler(async(req,res)=>{
+  const book = await Book.findByPk(req.params.id)
+  await book.destroy()
+  res.redirect('/books')
+}))
+
 
 module.exports = router;
